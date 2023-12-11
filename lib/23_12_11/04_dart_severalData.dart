f4347730e567cf23b7a63f78f1525ab973daa376
Sangin Han <hugedreamhd@gmail.com>
2023-12-11 21:41:54 +0900

    important

--- lib/23_12_11/04_dart_severalData.dart
+++ lib/23_12_11/04_dart_severalData.dart
@@ -36,19 +36,19 @@ class Department {
 
 void main() {
   String addEmployee = '{"name": "홍길동", "age": "41"}';
-  Map<String, dynamic> json = jsonDecode(addEmployee);
+  Map<String, dynamic> jsonEmployee = jsonDecode(addEmployee);
 
   Employee employee = Employee('홍길동', 41);
   Department department = Department('총무부', employee);
 
-  //employee 객체를 직렬화하여 저장
-  Map<String, dynamic> departmentMap = {
+  //employee 객체를 직렬화하여 저장 - 한줄로 갖고 오나봐
+  Map<String, dynamic> departmentLeader = {
     'name': department.name,
     'leader': employee.toJson(),
   };
 
   //직렬화 파일에 쓰기
   final file = File('lib/23_12_11/company.txt');
-  String jsonString = jsonEncode(departmentMap);
-  file.writeAsStringSync(jsonString);
+  String jsonWrite = jsonEncode(departmentLeader);
+  file.writeAsStringSync(jsonWrite);
 }
