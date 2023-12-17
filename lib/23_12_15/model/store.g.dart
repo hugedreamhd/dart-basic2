3d61c981e6f6cb34ff13743e97329f0a391123ec
Sangin Han <hugedreamhd@gmail.com>
2023-12-18 00:53:32 +0900

    important

---
+++ lib/23_12_15/model/store.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'store.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Strong _$StrongFromJson(Map<String, dynamic> json) => Strong(
+      json['name'] as String,
+    );
+
+Map<String, dynamic> _$StrongToJson(Strong instance) => <String, dynamic>{
+      'name': instance.name,
+    };
